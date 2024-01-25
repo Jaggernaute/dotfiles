30b1b5687874b39c2ca169fcdcac13526fd9c0e3
Sigmanificient <edhyjox@gmail.com>
2024-01-25 03:53:17 +0100

    Register toaster machine in flake

---
+++ toaster.nix
@@ -0,0 +1,45 @@
+{ inputs, system }: with inputs;
+let
+  username = "jaggi";
+in
+{
+  inherit system;
+
+  specialArgs = {
+    inherit username;
+    hostname = "Toaster";
+  };
+
+  modules =
+    let
+      home-manager-conf = {
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          users.${username} = import ./home;
+          extraSpecialArgs = {
+            inherit username system;
+          };
+        };
+      };
+
+      hosts-conf = {
+        networking.stevenBlackHosts.enable = true;
+      };
+
+      mod-nixhardware-lst = with nixos-hardware.nixosModules; [
+        common-pc-laptop
+        common-cpu-intel
+        common-pc-ssd
+      ];
+    in
+    [
+      ./system
+      ./hardware-configuration-laptop.nix
+    ] ++ [
+      home-manager.nixosModules.home-manager
+      home-manager-conf
+      hosts.nixosModule
+      hosts-conf
+    ] ++ mod-nixhardware-lst;
+}
