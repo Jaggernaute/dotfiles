da6d9118062b4811b38c85e5e8d035799cbb9458
Jaggernaute <lucas.videcoq1@gmail.com>
2024-04-02 22:28:15 +0200

    fix config to support the lack of battery

--- home/qtile/src/core/screens.py
+++ home/qtile/src/core/screens.py
@@ -8,22 +8,15 @@ from libqtile.config import Screen
 from core.bar import Bar
 
 gap = Gap(4)
-
 _cwd = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
-
 wallpaper_path = os.path.expanduser(f"~/assets/wall-{random.randint(21,22):02d}.png")
-
 if not os.path.exists(wallpaper_path):
     wallpaper_path = str((_cwd / ".." / ".." / "wall-22.png").absolute())
 
-screens = [
-    Screen(
-        top=Bar(i),
-        bottom=gap,
-        left=gap,
-        right=gap,
-        wallpaper=wallpaper_path,
-        wallpaper_mode="fill",
-    )
-    for i in range(2)
-]
+_gap = Gap(4)
+_screen_attr = dict(
+    bottom=_gap, left=_gap, right=_gap,
+    wallpaper=wallpaper_path,
+    wallpaper_mode="fill")
+
+screens = [Screen(top=Bar(i), **_screen_attr) for i in range(2)]
